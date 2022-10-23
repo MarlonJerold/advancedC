c0fc641deda587884e7ea5023b00ed0b96c34bb4
MarlonJerold <jeroldmarlon12@gmail.com>
2022-10-23 17:59:22 -0300

    Leitura do mapa e realização do movimento do @

--- game.c
+++ game.c
@@ -7,7 +7,7 @@ int linhas;
 int colunas;
 
 
-void libermapa(){
+void liberamapa(){
     for (int i = 0; i < linhas; i++){
         free(mapa[i]);
     }
@@ -37,13 +37,60 @@ void alocamapa(){
         mapa[i] = malloc(sizeof(char) * (colunas + 1));
     }
 }
-int main(){
-    lermapa();
-    
+void imprimirmapa(){
     for(int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
+}
+void move(char direcao){
+    int x;
+    int y;
 
-    libermapa();
+    for (int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            if (mapa[i][j] == '@'){
+                x = i;
+                y = j;
+                break;
+            }     
+        }
+    }
+
+    switch (direcao){
+    case 'a':
+        mapa[x][y-1] = '@'; 
+        break;
+    case 'w':
+        mapa[x-1][y] = '@';
+        break;
+    case 's':
+        mapa[x+1][y] = '@';
+        break;
+    case 'd':
+        mapa[x][y+1] = '@';
+        break;  
+    }  
+
+    mapa[x][y] = '.';  
+    
+
+}
+int acabou(){
+    return 0;
+}
+int main(){
+    lermapa();
+
+    do {
+
+        imprimirmapa();
+
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+
+    } while (!acabou());
+    
+    liberamapa();
 
 }
